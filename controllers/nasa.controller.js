93ee6a63e0e2f2d781760c97da85b86bc7bf7ae4
Elvis <provictor.ie@gmail.com>
2022-03-31 16:34:51 +0100

    NASA api routes.

--- controllers/nasa.controller.js
+++ controllers/nasa.controller.js
@@ -5,7 +5,7 @@ const { urlConstructor } = require("../utils/urlFunctions.js");
 
 const URL_APOD = "https://api.nasa.gov/planetary/apod";
 const URL_NEO = "https://api.nasa.gov/neo/rest/v1/feed";
-const URL_ASSET_BASE = "https//images-api.nasa.gov";
+const URL_EONET = "https://eonet.gsfc.nasa.gov/api/v3/events";
 
 const getAPOD = async (req, res) => {
   // Gets the Astronomy Picture of the Day from NASA's API
@@ -39,13 +39,14 @@ const getAPOD = async (req, res) => {
       copyright,
     });
   } catch (err) {
-    res.status(500).json({ success: false, issue: err });
+    res.status(500).json({ success: false, reason: err });
   }
 };
 
 const getNEO = async (req, res) => {
   /*
     Expectation: returns a list of the closest NEOs to the date specified.
+    Query params: start_date, end_date, limit
    */
   const { startDate, endDate } = req.query;
   const urlString = urlConstructor(URL_NEO, {
@@ -61,8 +62,26 @@ const getNEO = async (req, res) => {
     res.status(200).json({ success: true, element_count, near_earth_objects });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ success: false, err: err });
+    res.status(500).json({ success: false, reason: err });
   }
 };
 
-module.exports = { getAPOD, getNEO };
+const getEONET = async (req, res) => {
+  /* 
+    Query params: source, status, limit, days
+  */
+  const urlString = urlConstructor(URL_EONET, {
+    ...url.parse(req.url, true).query,
+  });
+
+  try {
+    const response = await needle("get", urlString);
+    const { events } = response.body;
+    res.status(200).json({ success: true, events });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, reason: err });
+  }
+};
+
+module.exports = { getAPOD, getNEO, getEONET };
--- routes/api/nasa.routes.js
+++ routes/api/nasa.routes.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
-const { getAPOD, getNEO } = require("../../controllers/nasa.controller.js");
+const {
+  getAPOD,
+  getNEO,
+  getEONET,
+} = require("../../controllers/nasa.controller.js");
 
 router.get("/nasa/apod", getAPOD);
 router.get("/nasa/neo", getNEO);
+router.get("/nasa/eonet", getEONET);
 
 module.exports = router;
