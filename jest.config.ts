996f858aee4ed60c6e7cc3f2b68584eb1171e2ef
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-18 21:54:35 +0100

    Jest-config edited

--- jest.config.ts
+++ jest.config.ts
@@ -145,6 +145,7 @@ export default {
 
     // The test environment that will be used for testing
     // testEnvironment: "jest-environment-node",
+    testEnvironment: "node",
 
     // Options that will be passed to the testEnvironment
     // testEnvironmentOptions: {},
@@ -153,10 +154,7 @@ export default {
     // testLocationInResults: false,
 
     // The glob patterns Jest uses to detect test files
-    // testMatch: [
-    //   "**/__tests__/**/*.[jt]s?(x)",
-    //   "**/?(*.)+(spec|test).[tj]s?(x)"
-    // ],
+    testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
     // testPathIgnorePatterns: [
