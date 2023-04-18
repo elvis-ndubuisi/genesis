ccb9f88bb40d3d6e3f73b5afb7eee008f99d5fe1
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-18 21:49:53 +0100

    Added config t-definitins

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("ts");
+require("dotenv").config();
+import config from "config";
+import express from "express";
+import helmet from "helmet";
+import routes from "./routes";
+import logger from "./helpers/logger";
+
+const app = express();
+
+const PORT = config.get<number>("port");
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(routes);
+
+app.listen(PORT, () => logger.info(`server started on ${PORT}`));
