abba225fedf5f0d40ec6836594335082530b94c4
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-24 17:48:19 +0100

    Server routes setup

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,6 @@ connectToMongo();
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(routes);
 
 app.listen(PORT, () => logger.info(`server started on ${PORT}`));
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,12 @@
 import express, { Response, Request, NextFunction } from "express";
 
+import salesRoutes from "./salesprit.routes";
+import cryptoRoutes from "./cryptpket.routes";
+
 const router = express.Router();
 
+router.use(salesRoutes);
+router.use(cryptoRoutes);
 router.get("/alive", (_, res: Response) => res.sendStatus(200));
 
 router.get("*", (_, res: Response, next: NextFunction) => {
